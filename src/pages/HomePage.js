3cb1ae4f82624ee906c1c243145651dc3fb9e004
denise williams <denisewilliams@denises-MacBook-Pro.local>
2021-06-14 12:40:38 -0400

    fixed an error

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import MainArea from '../components/MainArea';
 import DummyBlock from '../components/DummyBlock';
-import Footer from '../components/Footer';
+
 
 function HomePage(props) {
     return(
